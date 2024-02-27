68325516623b5e56072b71dce390470056f6107d
pipegoods <pipe.jaider@gmail.com>
2024-02-27 11:50:42 -0500

    Actualizar enlace de GitHub en el pie de página

--- src/footer.jsx
+++ src/footer.jsx
@@ -4,7 +4,7 @@ export function Footer() {
       <p>
         Hecho con el ❤️ por
         <a
-          href="https://github.com/pipegoods"
+          href="https://github.com/pipegoods/my-battery-social"
           target="_blank"
           rel="noopener noreferrer"
         >
